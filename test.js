fe6ca83401c596a31d871ca28613726395004035
David Yee <tangentialism@gmail.com>
2014-11-19 18:13:13 -0500

    Add tests for semantic cleanup

--- test.js
+++ test.js
@@ -36,4 +36,17 @@ for(var i = 0; i < ITERATIONS; ++i) {
   }
 }
 
+console.log('Checking semantic cleanup...');
+var verse1 = 'Particle man, particle man / Doing the things a particle can / What\'s he like? It\'s not important / Particle man';
+var verse2 = 'Triangle man, triangle man / Triangle man hates person man / They have a fight, triangle wins / Triangle man';
+var result = diff(verse1, verse2, true);
+var expected = googlediff.diff_main(verse1, verse2);
+googlediff.diff_cleanupSemantic(expected);
+
+if (!_.isEqual(result, expected)) {
+  console.log('Expected', expected);
+  console.log('Result', result);
+  throw new Error('Diff produced different semantic results.');
+}
+
 console.log("Success!");
